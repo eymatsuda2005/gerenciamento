c6532ab14bd01a98ad56ebc62ef9cbc2bb5d1686
eymatsuda2005 <eymatsuda2005@gmail.com>
2021-12-14 20:36:59 -0300

    Feito Enpoint clientes/{id} disponivel

--- src/main/java/com/eduardomatsuda/gerenciamento/domain/Cliente.java
+++ src/main/java/com/eduardomatsuda/gerenciamento/domain/Cliente.java
@@ -15,6 +15,7 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 
 import com.eduardomatsuda.gerenciamento.domain.enums.TipoCliente;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
 
 
 @Entity
@@ -30,6 +31,7 @@ public class Cliente implements Serializable{
 	private String cpdOuCnpj;
 	private Integer tipo;
 	
+	@JsonManagedReference
 	@OneToMany(mappedBy= "cliente")
 	private List<Endereco>enderecos = new ArrayList<>();
 	
